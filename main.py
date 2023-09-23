281bd0037d056721f247152331e2285c8c3ad049
Suryansh Shakya <suryanshsinghshakya1@gmail.com>
2023-09-24 01:02:27 +0530

    Designed Widget

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
 import MusicPlayer
-import Widget
+import widget
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
 
+def run_widget():
+    # Create a QApplication instance
+    app = QApplication(sys.argv)
+
+    # Create the main window
+    window = QMainWindow()
+    ui = widget.UI_widget()
+    ui.setupUi(window)
+
+    # Show the main window
+    window.show()
+
+    # Start the main event loop
+    sys.exit(app.exec_())
 
 if __name__ == "__main__":
     #MusicPlayer.main()
-    Widget.main()
+    #widget.widget.main()
+    run_widget()
 
 
 
